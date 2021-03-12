dcebbe5ae05a966f686bf4844516196c75740b3b
Gabriele La Delfa <gabrieleladelfa@192.168.1.56>
2021-03-12 16:42:59 +0100

    Aggiunta di un commento

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main (String [] args){
+        #stampa 
         System.out.println("Hello world\n");
         System.out.println("I hope that tomorrow will be a good day too\n");
         }
