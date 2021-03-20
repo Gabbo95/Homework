22abf2889ef1629e93c5814fce4ea97313a46b59
Gabbo95 <gabriele.ladelfa95@gmail.com>
2021-03-20 16:10:32 +0100

    Aggiunta di testo

--- TestJson/src/hello.java
+++ TestJson/src/hello.java
@@ -4,7 +4,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Ciao mondo");
+		System.out.println("Ciao mondo\n");
+		System.out.println("Oggi si prosegue alla grande");
 		
 	}
 
