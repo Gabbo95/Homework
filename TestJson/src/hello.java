2abec1e2405e809a013d368be8be772c235a3620
Gabbo95 <gabriele.ladelfa95@gmail.com>
2021-03-20 16:11:05 +0100

    Aggiunta di un commento

--- TestJson/src/hello.java
+++ TestJson/src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Testo da stampare
 		System.out.println("Ciao mondo\n");
 		System.out.println("Oggi si prosegue alla grande");
 		
